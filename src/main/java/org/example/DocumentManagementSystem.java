4237d1ac0207fa20c60dca713fa9cd530c296664
DESKTOP-1ROFC7Q\ksy13 <ksy13001@gmail.com>
2026-02-07 20:44:13 +0900

    4.4.4 임포터 구현과 등록

--- src/main/java/org/example/DocumentManagementSystem.java
+++ src/main/java/org/example/DocumentManagementSystem.java
@@ -1,8 +1,18 @@
 package org.example;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class DocumentManagementSystem {
+
+    private final Map<String, Importer> extensionToImporter = new HashMap<>();
+
+    public DocumentManagementSystem(){
+        extensionToImporter.put("jpg", new ImageImporter());
+        extensionToImporter.put("report", new ImageImporter());
+        extensionToImporter.put("letter", new ImageImporter());
+    }
 
     void importFile(String path){
 
---
+++ src/main/java/org/example/LetterImporter.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import java.io.File;
+import java.io.IOException;
+
+public class LetterImporter implements Importer{
+    @Override
+    public Document importFile(File file) throws IOException {
+        return null;
+    }
+}
