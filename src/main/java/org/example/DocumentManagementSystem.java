f9b28d875a12d72105043976d2f96bdd26322cfd
DESKTOP-1ROFC7Q\ksy13 <ksy13001@gmail.com>
2026-02-07 20:59:09 +0900

    4.5 LSP: 하위형식에서 선행조건을 더할 수 없음
    - Importer 구현은 임포트하는 파일이 존재하며 확장자가 존재하고 읽을 수 있다는 선행 조건이 필요하다

--- src/main/java/org/example/DocumentManagementSystem.java
+++ src/main/java/org/example/DocumentManagementSystem.java
@@ -1,11 +1,16 @@
 package org.example;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class DocumentManagementSystem {
 
+    private final List<Document> documents = new ArrayList<>();
     private final Map<String, Importer> extensionToImporter = new HashMap<>();
 
     public DocumentManagementSystem(){
@@ -14,7 +19,23 @@ public class DocumentManagementSystem {
         extensionToImporter.put("letter", new ImageImporter());
     }
 
-    void importFile(String path){
+    void importFile(String path) throws IOException {
+        final File file = new File(path);
+        if(!file.exists()){
+            throw new FileNotFoundException(path);
+        }
+
+        final int separatorIndex = path.lastIndexOf('.');
+        if (separatorIndex == -1) {
+            throw new UnknownFileTypeException("No extension found For file: "+ path);
+        }
+        final String extension = path.substring(separatorIndex + 1);
+        final Importer importer = extensionToImporter.get(extension);
+        if(importer == null){
+            throw new UnknownFileTypeException("Unknown file type: "+ extension);
+        }
+        final Document document = importer.importFile(file);
+        documents.add(document);
 
     }
 
